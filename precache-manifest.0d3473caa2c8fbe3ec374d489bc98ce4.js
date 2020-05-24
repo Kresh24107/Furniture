6626fe0301c763036a7938573afd950c5272d307
Vadim Sirenko <vadim7ko@gmail.com>
2020-05-24 22:39:32 +0300

    Updates

---
+++ precache-manifest.0d3473caa2c8fbe3ec374d489bc98ce4.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a035d8277d0e066a2cbfba47eb9fa141",
+    "url": "/furniture/index.html"
+  },
+  {
+    "revision": "d62fa06008c7bd3f5342",
+    "url": "/furniture/static/css/main.b6ff0972.chunk.css"
+  },
+  {
+    "revision": "2711d4a68ce6304ccc42",
+    "url": "/furniture/static/js/2.ba454d3b.chunk.js"
+  },
+  {
+    "revision": "501f379103cbfcb5ff11211dd39a28a2",
+    "url": "/furniture/static/js/2.ba454d3b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d62fa06008c7bd3f5342",
+    "url": "/furniture/static/js/main.1a83bbce.chunk.js"
+  },
+  {
+    "revision": "7d592a681f57396750a1",
+    "url": "/furniture/static/js/runtime-main.dabcebe7.js"
+  },
+  {
+    "revision": "876e4985b0086e140ef9b3fa1354175b",
+    "url": "/furniture/static/media/Group_2.876e4985.png"
+  },
+  {
+    "revision": "95fc19318cf6aa003e6b3cf1b0e0d378",
+    "url": "/furniture/static/media/armchair.95fc1931.png"
+  },
+  {
+    "revision": "2917ab9cc1993163bdc01f252a202ec0",
+    "url": "/furniture/static/media/ellipse-armchair.2917ab9c.png"
+  }
+]);
